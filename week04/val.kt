b1c34097fb28bab17d4a98e9914eb4f8649aa500
JoseLedesma122 <157508170+JoseLedesma122@users.noreply.github.com>
2024-02-01 10:46:01 -0600

    Var and val

---
+++ week04/val.kt
@@ -0,0 +1,19 @@
+// VarAndVal/Task1.kt
+package varAndValExercise1
+
+fun main() {
+  // TODO
+
+    val first = "Jose"
+    val last = "Ledesma"
+    var age = 16
+
+    println("Hello world! My name is $first $last, and i'm $age years old.")
+    age = age - 5
+    println("and you believed i'm $age years old")
+
+    print("What day is it today? ")
+    val day = readLine()!!
+    println(day)
+
+}
