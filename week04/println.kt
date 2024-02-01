9906b9e5ec08e1a19c82560aa78b315898d698e5
JoseLedesma122 <157508170+JoseLedesma122@users.noreply.github.com>
2024-02-01 09:55:19 -0600

    created println code

---
+++ week04/println.kt
@@ -0,0 +1,12 @@
+// HelloWorld/Task1.kt
+package helloWorldITSE1329
+
+
+
+// TODO
+
+fun main() {
+    println("Hello World")
+    println("My name is Jose")
+    println("I'm taking the course ITSE-1329")
+}
