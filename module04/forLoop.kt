c0838a57b9ca0904ca4930c140a199c9b70f3504
JoseLedesma122 <157508170+JoseLedesma122@users.noreply.github.com>
2024-02-06 11:32:51 -0600

    Create forLoop.kt

---
+++ module04/forLoop.kt
@@ -0,0 +1,20 @@
+fun main(){
+    for (i in 1..3) {
+        println("hey $i!")
+    }
+    val range1 = 1..10
+    val range2 = 0 until 10
+    println(range1)
+    println(range2)
+
+    var sum = 0
+    for (n in 10..100){
+        sum += n
+        println(sum)
+    }
+    println("sum = $sum")
+    }
+
+
+
+
