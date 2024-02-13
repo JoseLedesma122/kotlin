474ed215e774894f8d8c285b15774dab9df7e2ed
JoseLedesma122 <157508170+JoseLedesma122@users.noreply.github.com>
2024-02-13 11:18:22 -0600

    annual icome name age

---
+++ module04/NameAgeAnnualIncome.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Hello World!")
+var name: String = "Jose Ledesma"
+var age: Int = 16
+var annualPay: Double = 999999.99
+
+println("My name is " + name + ", my age is " + age + ".")
+println("I hope to earn " + annualPay + " per year")
+}
