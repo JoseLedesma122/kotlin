eebeb3a69960f9981ae1618890e07b68425f1787
JoseLedesma122 <157508170+JoseLedesma122@users.noreply.github.com>
2024-02-15 10:28:45 -0600

    Advertising Conditional

---
+++ module04/AdvertisingConditional.kt
@@ -0,0 +1,9 @@
+fun main() {
+  val friendCount = 510
+if (friendCount < 500){
+    println( "Buy Cat Food At 20% Off")
+} else { if (friendCount > 500 || friendCount == 500)
+println("Adopt a Dog Today!")
+}
+
+}
